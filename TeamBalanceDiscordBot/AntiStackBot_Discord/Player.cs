85017daf8e8ab272c9a43fb7c067b5a1b67e6269
Ezcoo <4085301+Ezcoo@users.noreply.github.com>
2024-02-25 16:49:37 +0200

    Update default skill value from 4.0 to 8.0 to reflect new score counting

--- TeamBalanceDiscordBot/AntiStackBot_Discord/Player.cs
+++ TeamBalanceDiscordBot/AntiStackBot_Discord/Player.cs
@@ -25,7 +25,7 @@ namespace A2WASPDiscordBot_Windows_App
             this.maxNumberOfScores = 500;
             this.ticksThresholdForCountingSkill = 40;
             this.defaultNumberOfTicks = 10;
-            this.defaultScore = 40;
+            this.defaultScore = 80;
             this.side = Side.NONE;
             this.currentTicksCached = this.defaultNumberOfTicks;
             existsInDatabase(this.id);
